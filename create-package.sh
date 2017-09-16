6dd58fbeede6eb10df2861e15c0038d37b3b5ad1
TheElectronWill <theelectronwill@gmail.com>
2017-09-16 18:46:49 +0200

    Use colors + cleaner messages

--- create-package.sh
+++ create-package.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Author: TheElectronWill
 # This script downloads the latest version of gitkraken for linux, and creates a package with rpmbuild.
+source terminal-colors.sh
 
 rpm_dir="$PWD/RPMs"
 work_dir="$PWD/work"
@@ -25,41 +26,44 @@ exec_file="$downloaded_dir/$exec_name"
 
 # It's a bad idea to run rpmbuild as root!
 if [ "$(id -u)" = "0" ]; then
-	echo '------------------------ WARNING ------------------------'
-	echo 'This script should NOT be executed with root privileges!'
-	echo 'Building rpm packages as root is dangerous and may harm the system!'
-	echo 'Actually, badly written RPM spec files may execute dangerous command in the system directories.'
-	echo 'So it is REALLY safer not to run this script as root.'
-	echo 'If you still want to run this script as root, type "do it!" within 5 seconds (type anything else to exit):'
-	read -t 5 -n 6 -p '> Do you really want to do it (not recommended)? ' answer
+	disp "$red_bg------------------------ WARNING ------------------------\n\
+	      This script should NOT be executed with root privileges!\n\
+	      Building rpm packages as root is dangerous and may harm the system!\n\
+	      Actually, badly written RPM spec files may execute dangerous command in the system directories.\n\
+	      So it is REALLY safer not to run this script as root.\n\
+	      If you still want to continue, type \"do it!\" within 5 seconds (type anything else to exit)."
+	disp "------------------------ WARNING ------------------------$reset$bold"
+	read -t 5 -p '> Do you really want to do it (not recommended)? ' answer
 	if [ "$answer" != "do it!" ]; then
 		exit
 	fi
-	echo '------------------------ WARNING ------------------------'
-	echo
+	style $reset
 fi
 
 # Checks that the rpmbuild package is installed.
 if ! type 'rpmbuild' > /dev/null; then
 	echo 'You need the rpm development tools to create rpm packages.'
-	read -n 1 -p '> Do you want to install the rpmdevtools package now? [y/N]' answer
+	style $bold
+	read -n 1 -p '> Install the rpmdevtools package now? [y/N]: ' answer
 	echo
 	case "$answer" in
 		y|Y)
 			sudo -p 'Enter your password to install rpmdevtools: ' dnf install rpmdevtools
 			;;
 		*) 
-			echo "Ok, I won't install rpmdevtools."
+			echo "${reset}Ok, I won't install rpmdevtools."
 			exit
 	esac
+	style $reset
 else
-	echo "rpmbuild detected!"
+	disp "${green}rpmbuild detected.$reset"
 fi
 
 # Checks that the version is given as a parameter.
 if [ $# -ne 1 ]; then
+	disp "${red}Wrong number of parameters!$reset"
 	echo 'Usage: create-package.sh gitkraken_version'
-	echo '   - gitkraken_version is for instance 2.1 or 3.0.0'
+	echo ' - gitkraken_version: for instance 2.1 or 3.0.0'
 	exit
 fi
 
@@ -71,15 +75,17 @@ download_gitkraken() {
 
 # Asks the user if they want to remove the specified directory, and removes it if they want to.
 ask_remove_dir() {
-	read -n 1 -p "> Do you want to remove the directory \"$1\"? [y/N]" answer
+	style $reset$bold
+	read -n 1 -p "> Remove the directory \"$1\"? [y/N]: " answer
 	echo
+	style $reset
 	case "$answer" in
 		y|Y)
 			rm -r "$1"
-			echo "\"$1\" directory removed."		
+			echo "Directory removed."		
 			;;
 		*)
-			echo "Ok, I won't remove it."
+			echo "Directory not removed."
 	esac
 	echo
 }
@@ -101,14 +107,17 @@ cd "$work_dir"
 # Downloads gitkraken if needed.
 if [ -e "$archive_name" ]; then
 	echo "Found the archive \"$archive_name\"."
-	read -n 1 -p '> Do you want to use this archive instead of downloading a new one? [y/N]' answer
+	style $reset$bold
+	read -n 1 -p '> Use this archive instead of downloading a new one? [y/N]: ' answer
 	echo
+	style $reset
 	case "$answer" in
 		y|Y)
-			echo 'Ok, I will use this archive.'
+			echo 'Existing archive selected.'
 			;;
 		*)
 			rm "$archive_name"
+			echo 'Existing archive removed.'
 			download_gitkraken
 	esac
 else
@@ -130,12 +139,13 @@ cp "$desktop_model" "$desktop_file"
 sed "s/@version/$version_number/; s/@icon/$icon_name/; s/@exe/$exec_name/" "$desktop_model" > "$desktop_file"
 
 
-echo 'Creating the RPM package (this may take a while)...'
+disp "${yellow}Creating the RPM package (this may take a while)..."
 rpmbuild -bb --quiet --nocheck "$spec_file" --define "_topdir $work_dir" --define "_rpmdir $rpm_dir"\
 	--define "arch $arch" --define "downloaded_dir $downloaded_dir" --define "desktop_file $desktop_file"\
 	--define "version_number $version_number" --define "exec_name $exec_name"
 
-echo
-echo '------------------------- Done! -------------------------'
-echo "The RPM package is located in the \"RPMs/$arch\" folder."
+disp "${bgreen}Done!${reset_font}"
+disp "The RPM package is located in the \"RPMs/$arch\" folder."
+disp '----------------'
+
 ask_remove_dir "$work_dir"
